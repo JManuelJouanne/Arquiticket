76cda5c706ee24252308c852e0f2fd9080d0ef7f
Pablok98 <pjkipreos@uc.cl>
2023-04-30 17:35:27 -0400

    Fix CORS from publisher

--- publisher/main.py
+++ publisher/main.py
@@ -4,7 +4,17 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 import _thread
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def on_connect(client, userdata, flags, rc):
