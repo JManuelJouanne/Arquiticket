c158975917bd006f997205227259a3b6d75914e6
Corubio <corubio@uc.cl>
2023-06-30 18:32:07 -0400

    fix:mailer

--- api/routes/events.py
+++ api/routes/events.py
@@ -1,10 +1,12 @@
 from fastapi import Request, APIRouter, Depends
 from db.get import get_db
 from db import crud, schemas
-from mailing import mailer
+from mailing import send_notification
 from requests.models import Response
 from sqlalchemy.orm import Session
 import uuid
+import boto3
+import json
 
 
 events = APIRouter()
@@ -33,13 +35,13 @@ def create_event(event: schemas.EventCreate, db: Session = Depends(get_db)):
     return crud.create_event(db=db, event=event)
 
 
-@events.post("/events/user/buy/", responses={204: {"model": None}}, )
+@events.post("/events/user/buy/", responses={204: {"model": None}})
 async def validate_events(info: Request, db: Session = Depends(get_db)):
     uuid_request = generate_uuid()
     payload = await info.json()
 
     if (int(crud.get_money(db, user_id=payload["user_id"]).money)
-            < int(payload["quantity"] * crud.get_event(db, event_id=payload["event_id"]).price)):
+            < int(payload["quantity"] * int(crud.get_event(db, event_id=payload["event_id"]).price))):
         response = Response()
         response.status_code = 400
         return {"detail": "Not enough money"}
@@ -50,14 +52,45 @@ async def validate_events(info: Request, db: Session = Depends(get_db)):
             user_id=payload["user_id"],
             event_id=payload["event_id"],
             quantity=payload["quantity"],
-            status=1))
+            status=1,
+            link=""
+        ))
 
         crud.use_money(db=db, user_id=payload["user_id"], quantity=payload["quantity"] *
                        crud.get_event(db, event_id=payload["event_id"]).price)
         crud.update_our_event_less(db=db, event_id=payload["event_id"],
                                    tickets=payload["quantity"])
+        # Mailing
+        ticket = crud.get_ticket(db, uuid_request)
+        event = crud.get_event(db, payload["event_id"])
+        # lambda
+        data = {
+            "name": event.name,
+            "user": ticket.user_id,
+            "date": event.date,
+            "id": event.event_id,
+            "request_id": ticket.request_id,
+        }
+        session = boto3.Session(
+            region_name='us-east-2',
+            aws_access_key_id='AKIAWTW2MNNWBVCCU3QL',
+            aws_secret_access_key='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'
+        )
 
-        mailer(uuid_request, created_ticket["event_id"])
+        lambda_client = session.client('lambda')
+
+        response = lambda_client.invoke(
+            FunctionName='generar_ticket',
+            Payload=json.dumps(data)
+        )
+
+        result = response['Payload'].read().decode('utf-8')
+        result = json.loads(result)["body"]
+
+        crud.update_ticket_link(db, request_id=uuid_request, link=result["url"])
+        # URL para descargar las entradas de AWS Lambda
+        url = result["url"].replace(" ", "")
+        send_notification(ticket=ticket, event=event, url=url)
 
         response = Response()
         response.headers = {"Access-Control-Allow-Origin": "*"}
